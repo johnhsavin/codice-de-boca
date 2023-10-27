6559e9e011bce4d4aacc1ac0d200551e3464462e
John Savin <johnsavin@proton.me>
2023-10-27 12:02:29 -0400

    changes to codice

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,40 +1,49 @@
+import { useState } from 'react'
 
-import { useState } from "react"
-import SingleMenuComponent from "./SingleMenuComponent"
+function SingleMenuComponent(props) {
+	console.log('props ->', props)
+	return (
+		<div className='menu-item'>
+			<h3>{props.title}</h3>
+			<p>{props.description}</p>
+		</div>
+	)
+}
+
 export default function Home() {
-  const [menuItems, setMenuItems] = useState(["hotdog", "burger"])  // =[stateVar, setter
-  // 1. fetch data 
-  // 2. get array of objects
-  // 3. render each object.
-  // 3.1 create a state variable
-  // 3.2 put inside state variable the array of objects
-  // 3.3 display each item in DOM
-  // 3.3.1 map over the array of objects
-  // 3.3.2 return each item so that it displays on the browser
+	const [menuItems, setMenuItems] = useState() // = [stateVar, setter]
 
-  console.log(menuItems)
+	// 1. fetch data - DONE
+	// 2. get array of objects - DONE
 
-  const handleBtnClick = () => {
-    console.log("hey class from a function")
+	// 3. render each object
+	// 3.1 create a state variable - DONE
+	// 3.2 put inside state variable the array of objects - DONE
+	// 3.3 display each item in dom
+	// 3.3.1 Map over the array of objects
+	// 3.3.2 return each item so that it displays on the browser
 
-    fetch("https://codice-boca.web.app/menu")
-      .then(res => res.json())    // contacting the api
-      .then(data => setMenuItems(data)) // get clean data 
-      .catch(x => console.error(x))
-  }
+	const handleBtnClick = () => {
+		fetch('https://codice-boca.web.app/menu')
+			.then(res => res.json()) // contacting the API
+			.then(data => setMenuItems(data)) // get clean data form res
+			.catch(x => console.error(x))
+	}
 
-  return (
-    <>
-      <button onClick={() => handleBtnClick()}>get data</button>
-      {menuItems.map((singleItem) => {
-        return (
-          <>
-          <SingleMenuComponent title={singleItem.title} />
-            {/* <h2>{singleItem.title}</h2>
-        <p>{singleItem.description}</p> {*/}
-          </>
-        )
-      })}
-    </>
-  )
+	return (
+		<section className='menu-items'>
+			{!menuItems
+					// if no menu items, show button
+					? <button onClick={() => handleBtnClick()}>get data</button>
+					// otherwise show menu items
+					: menuItems.map(singleItem => {
+							return (
+									<SingleMenuComponent
+										title={singleItem.title}
+										description={singleItem.description} />
+							)
+						})
+			}
+		</section>
+	)
 }
--- src/components/SingleMenuComponent.jsx
+++ src/components/SingleMenuComponent.jsx
@@ -1,6 +1,9 @@
 export default function SingleMenuComponent(props){
   console.log(props)
   return (
+    <>
     <h3>{props.title}</h3>
+    <p>{props.description}</p>
+    </>
   )
 }
